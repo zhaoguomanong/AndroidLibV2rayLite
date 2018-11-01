49adc4eabcad627603a910aeae51ce1058c7b3c5
2dust <qingyede@gmail.com>
2018-11-01 14:39:18 +0800

    sort

--- CoreI/Status.go
+++ CoreI/Status.go
@@ -22,6 +22,20 @@ func (v *Status) getDataDir() string {
 	var datadir = "/data/data/com.v2ray.ang/"
 	if v.PackageName != "" {
 		datadir = "/data/user/" + strconv.Itoa(os.Getuid()/100000) + "/" + v.PackageName + "/"
+		if !Exists(datadir) {
+			datadir = "/data/data/" + v.PackageName + "/"
+		}
 	}
 	return datadir
+}
+
+func Exists(path string) bool {
+	_, err := os.Stat(path)
+	if err != nil {
+		if os.IsExist(err) {
+			return true
+		}
+		return false
+	}
+	return true
 }

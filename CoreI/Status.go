b4c82b1e52efd3ffd4d4ca704f11d873ee785f98
2dust <qingyede@gmail.com>
2018-12-18 11:41:47 +0800

    update12

--- CoreI/Status.go
+++ CoreI/Status.go
@@ -48,8 +48,8 @@ func (v *Status) GetOvertureArgs() []string {
 	var dynaArr []string
 	dynaArr = append(dynaArr, "-c")
 	dynaArr = append(dynaArr, v.PackageName + "config.json")
-	dynaArr = append(dynaArr, "-l")
-	dynaArr = append(dynaArr, v.PackageName + "overture.log")
+	//dynaArr = append(dynaArr, "-l")
+	//dynaArr = append(dynaArr, v.PackageName + "overture.log")
 	
 	return dynaArr
 }

b43a0379af09cc584eebbb696c77681850c26d54
cutelua <33216131+cutelua@users.noreply.github.com>
2019-05-09 18:41:07 +0800

    add compile tun2socks script

---
+++ compile-tun2socks.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Set magic variables for current file & dir
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
+__base="$(basename ${__file} .sh)"
+
+
+
+TMPDIR=$(mktemp -d)
+
+cp $__dir/tun2socks.mk $TMPDIR
+cd $TMPDIR
+git clone --depth=1 https://github.com/shadowsocks/badvpn.git
+git clone --depth=1 https://github.com/shadowsocks/libancillary.git
+
+$NDK_HOME/ndk-build \
+	NDK_PROJECT_PATH=. \
+	APP_BUILD_SCRIPT=./tun2socks.mk \
+	APP_ABI=all \
+	APP_PLATFORM=android-18 \
+	NDK_LIBS_OUT=$TMPDIR/libs \
+	NDK_OUT=$TMPDIR/tmp \
+	APP_SHORT_COMMANDS=false LOCAL_SHORT_COMMANDS=false -B -j4
+
+install -v -m755 libs/armeabi-v7a/tun2socks  $__dir/shippedBinarys/ArchDep/arm/ 
+install -v -m755 libs/arm64-v8a/tun2socks    $__dir/shippedBinarys/ArchDep/arm64/
+install -v -m755 libs/x86/tun2socks          $__dir/shippedBinarys/ArchDep/386/ 
+install -v -m755 libs/x86_64/tun2socks       $__dir/shippedBinarys/ArchDep/amd64/ 
+
